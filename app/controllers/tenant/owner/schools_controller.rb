0962faca27314626a46d672f630d2dd2777f6993
gian910147 <gian910147@gmail.com>
2022-06-06 03:06:34 +0800

    update subdomain(WIP)

---
+++ app/controllers/tenant/owner/schools_controller.rb
@@ -0,0 +1,26 @@
+class Tenant::Owner::SchoolsController < Tenant::BaseController
+  layout "owner"
+  before_action :find_teacher, only: [:edit, :update]
+  def edit; end
+
+  def update
+    if @teacher.update(school_params)
+      session.clear
+      redirect_to teacher_auto_sign_in_url(subdomain: current_teacher.subdomain)
+    else
+      render :edit
+    end
+
+  end
+
+
+  private
+  def school_params
+    params.require(:teacher).permit(:school_name, :avatar, :subdomain, :email, :username)
+  end
+
+  def find_teacher
+    @teacher = current_teacher
+  end
+
+end
