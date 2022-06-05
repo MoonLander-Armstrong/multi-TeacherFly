943736209f31041f052287742e4ebb29d1778053
gian910147 <gian910147@gmail.com>
2022-06-05 19:01:11 +0800

    nav上的 我的課程

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -2,13 +2,13 @@ class SubscriptionMailer < ApplicationMailer
   def registered_member(student, teacher)
     @student = student
     @teacher = teacher
-    mail to: student.email, subject: "您已經成為了TeacherFly的會員了"
+    mail to: @student.email, subject: "您已經成為了TeacherFly的會員了"
   end
 
   def give_away_subscription(student, teacher, order)
     @student = student
     @teacher = teacher
     @order = order
-    mail to: student.email, subject: "您已經獲得了一堂課程"
+    mail to: @student.email, subject: "您已經獲得了一堂課程"
   end
 end
