58668dcfeec627f02c6e19735485ccfa0197c6f6
gian910147 <gian910147@gmail.com>
2022-06-05 11:52:00 +0800

    backstage commplete

--- app/controllers/tenant/owner/orders_controller.rb
+++ app/controllers/tenant/owner/orders_controller.rb
@@ -4,7 +4,7 @@ class Tenant::Owner::OrdersController < Tenant::BaseController
   def index
     authorize :order
     @courses = Course.all
-    @orders = Order.includes(:user).where(course: @courses).paid.order(id: :desc)
+    @orders = Order.includes(:student).where(course: @courses).paid.order(id: :desc)
 
     if params[:days] && params[:days] != '0'
       @order_option = params[:days]
