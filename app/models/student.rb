23da3ecd7a481dd6fe34e58eae9924fef6fa75f4
gian910147 <gian910147@gmail.com>
2022-06-10 01:59:05 +0800

    mail內容修改

--- app/models/student.rb
+++ app/models/student.rb
@@ -18,7 +18,7 @@ class Student < ApplicationRecord
   has_one_attached :avatar, dependent: :destroy
 
   # scope
-  scope :ordered, -> { includes(:orders).order('orders.price desc') }
+  scope :ordered, -> { includes(:orders).order("orders.price DESC") }
 
   def bought?(course)
     orders.where(status: "paid").exists?(course_id: course.id)
