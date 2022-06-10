00adeb1b9a6ff9f63f2d1356ae6e2c478af4f48a
gian910147 <gian910147@gmail.com>
2022-06-10 19:07:32 +0800

    last commit

--- app/controllers/tenant/pages_controller.rb
+++ app/controllers/tenant/pages_controller.rb
@@ -10,4 +10,9 @@ class Tenant::PagesController < Tenant::BaseController
     sign_in MultiTenantSupport.current_tenant
     redirect_to owner_courses_path, notice: "更新成功"
   end
+
+  def auto_sign_out
+    sign_out current_teacher
+    redirect_to teacher_log_out_url(subdomain: "")
+  end
 end
